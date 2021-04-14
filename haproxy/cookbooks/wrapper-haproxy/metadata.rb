79ed528b835bc3df610ee49375f224de97f51a24
eduardobaitello <eduardobaitello@gmail.com>
2021-04-13 21:57:07 -0300

    Make haproxy servers configuration dynamic

--- haproxy/cookbooks/wrapper-haproxy/metadata.rb
+++ haproxy/cookbooks/wrapper-haproxy/metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Nobody'
 maintainer_email 'nobody@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures haproxt'
-version '0.2.0'
+version '0.3.0'
 chef_version '>= 14.0'
 
 depends 'haproxy', '~> 9.1.0'
--- haproxy/cookbooks/wrapper-haproxy/recipes/default.rb
+++ haproxy/cookbooks/wrapper-haproxy/recipes/default.rb
@@ -11,13 +11,28 @@ haproxy_frontend 'http-in' do
   default_backend 'servers'
 end
 
+# Using hardcoded IPs
+# haproxy_backend 'servers' do
+#   server [
+#     'web1 192.168.10.43:80 maxconn 32',
+#     'web2 192.168.10.44:80 maxconn 32',
+#   ]
+#   # https://github.com/sous-chefs/haproxy/issues/274
+#   # notifies :reload, 'haproxy_service[haproxy]', :immediately
+# end
+
+# Using search for dynamic nodes
+all_web_nodes = search('node', 'role:web')
+
+servers = []
+
+all_web_nodes.each do |web_node|
+  server = "#{web_node['hostname']} #{web_node['ipaddress']}:80 maxconn 32"
+  servers.push(server)
+end
+
 haproxy_backend 'servers' do
-  server [
-    'web1 192.168.10.43:80 maxconn 32',
-    'web2 192.168.10.44:80 maxconn 32',
-  ]
-  # https://github.com/sous-chefs/haproxy/issues/274
-  # notifies :reload, 'haproxy_service[haproxy]', :immediately
+  server servers
 end
 
 haproxy_service 'haproxy' do
